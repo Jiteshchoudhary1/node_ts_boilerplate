2886c8f4a3fa6302a3261f8dda4fe59c3245552b
jiteshchoudhary2 <jitesh.choudhary@noborderz.com>
2023-07-13 16:11:40 +0530

    update the code

--- src/api/database/util/index.ts
+++ src/api/database/util/index.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 export async function dbConnection() {
     // try {
-        mongoose.set('debug', true);
-        return await mongoose.connect(process.env.DB_URL || 'default');
+    mongoose.set('debug', true);
+    return await mongoose.connect(process.env.DB_URL || 'default');
     // } catch (error: any) {
     //     throw Error(error);
     // }
